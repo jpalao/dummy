23069ca0fe9728659be1cbb6d8b81b6c5b871569
Jose Palao <jose@palao.com>
2016-11-28 16:28:07 +0100

    added dummy files

---
+++ Dummy.h
@@ -0,0 +1,13 @@
+//
+//  Dummy.h
+//  dummy
+//
+//  Created by jose on 28/11/16.
+//  Copyright © 2016 jose. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dummy : NSObject
+
+@end
---
+++ Dummy.m
@@ -0,0 +1,13 @@
+//
+//  Dummy.m
+//  dummy
+//
+//  Created by jose on 28/11/16.
+//  Copyright © 2016 jose. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
